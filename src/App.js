e2d9b10a9c4d4723e19effce6673f74a62599b65
Saif Ullah <saifu0905@gmail.com>
2024-12-25 10:07:14 +0500

    Chatbot 80% completed, Landing page almost completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Newsletter from './components/Newsletter';
+import Dashboard from './components/chatbot/Chatbot';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       {/* <Newsletter /> */}
       {/* <Cards /> */}
       {/* <Footer /> */}
+      <Dashboard />
     </div>
   );
 }
